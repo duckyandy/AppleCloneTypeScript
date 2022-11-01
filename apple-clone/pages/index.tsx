c7acf7754f6ba2dcb1d28bc4020896f900e8ac0a
Andy Kwak <akandykwak@gmail.com>
2022-11-01 22:46:00 +0900

    day 1 basic header and some sanity

---
+++ apple-clone/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Apple Redesign</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <h1>Apple Redesign</h1>
+    </div>
+  );
+};
+
+export default Home;
